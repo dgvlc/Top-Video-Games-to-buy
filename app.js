6dfa5c2e9366e9a7265fd55816c6289570254845
diego <dgvlc@yahoo.com>
2021-06-24 13:51:09 -0400

    looking to render images

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
-const DOMAIN = 'https://www.giantbomb.com/api/reviews/';
-const API_KEY = 'dafa211aea44c6db2e94a706b672a5af7ce923e6';
-const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&format=json`;
 
 
+ const reviewUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/reviews/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
+// const searchUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/search/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
+ const baseUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/games/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
 
-// const reviewUrl = 'https://www.giantbomb.com/api/reviews/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6'
-// const searchUrl = 'https://www.giantbomb.com/api/search/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6'
- const baseUrl = 'http://www.giantbomb.com/api/game/3030-38207/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=jsonp&field_list=name,description'
-const generate = document.querySelector('#list-generator')
+const container = document.querySelector('.container-list')
 
 const videoGamesStarter = async () => {
   try {
+    const imageGet = await axios.get(baseUrl)
+    console.log(imageGet.data.results)
 
-    //con
-    const response = await axios.get(baseUrl)
-    console.log(response)
+    const response = await axios.get(reviewUrl)
+    //console.log(response.data.results)
+    const data = response.data.results
+    
+    for (let i = 0; i < 50; i++)
+    {
+      const score = response.data.results[i].score
+      if (score === 5 ) {
+        renderGames(data[i])
+        //console.log(data[i])
+      }
+        }
 
   } catch (error) {
     console.log(error)
   }
 }
-videoGamesStarter()
 
-// renderGames() {
 
-// }
+function renderGames(results) {
+  let gameData = `
+  <h1>Title: ${results.game.name}</h1>
+  `
+  container.insertAdjacentHTML('beforeend', gameData)
+  return gameData
 
-generate.addEventListener('click', (e) => {
-  e.preventDefault()
-  videoGamesStarter()
-})
+  }
+
+// generate.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   
+//   videoGamesStarter()
+// })
+
+document.getElementById("list-generator").addEventListener('click', videoGamesStarter())
 
