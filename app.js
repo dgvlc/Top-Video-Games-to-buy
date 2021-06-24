a620c4ddb2f4ecb5b7183a68112a7b0ab2f59867
diego <dgvlc@yahoo.com>
2021-06-24 14:45:19 -0400

    api working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+const api = '?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
  const reviewUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/reviews/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
 // const searchUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/search/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
  const baseUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/games/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
@@ -8,8 +8,6 @@ const container = document.querySelector('.container-list')
 
 const videoGamesStarter = async () => {
   try {
-    const imageGet = await axios.get(baseUrl)
-    console.log(imageGet.data.results)
 
     const response = await axios.get(reviewUrl)
     //console.log(response.data.results)
@@ -29,14 +27,28 @@ const videoGamesStarter = async () => {
   }
 }
 
-
-function renderGames(results) {
-  let gameData = `
+async function renderGames(results)  {
+  console.log(results.game.api_detail_url)
+  const gameUrl = results.game.api_detail_url
+  const imageGet = `https://floating-escarpment-65266.herokuapp.com/${gameUrl}${api}`
+  let currentGame = {
+   
+ }
+ try {
+  const response = await axios.get(imageGet)
+  currentGame = response.data.results
+   console.log(currentGame)
+   let gameData = `
   <h1>Title: ${results.game.name}</h1>
+  <img src="${currentGame.image.icon_url}"/>
   `
   container.insertAdjacentHTML('beforeend', gameData)
   return gameData
 
+
+} catch (error) {
+  console.log(error)
+}  
   }
 
 // generate.addEventListener('click', (e) => {
@@ -45,5 +57,8 @@ function renderGames(results) {
 //   videoGamesStarter()
 // })
 
-document.getElementById("list-generator").addEventListener('click', videoGamesStarter())
+document.getElementById("list-generator").addEventListener('click', (e) => {
+ e.preventDefault()
+  videoGamesStarter()
+})
 
