1258faddaf6165e669dcf5042a55747d8237bee0
diego <dgvlc@yahoo.com>
2021-06-25 17:31:27 -0400

    still working on somethings

--- app.js
+++ app.js
@@ -4,61 +4,79 @@ const api = '?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
 // const searchUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/search/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
  const baseUrl = 'https://floating-escarpment-65266.herokuapp.com/www.giantbomb.com/api/games/?api_key=dafa211aea44c6db2e94a706b672a5af7ce923e6&format=json'
 
-const container = document.querySelector('.container-list')
+const containerList = document.querySelector(".container-list")
+const container = document.querySelectorAll('.container')
+const holders = document.querySelectorAll('.holder')
+const nameDiv = document.querySelectorAll('.name-header')
+const descDiv = document.querySelectorAll('.desc-footer')
 
 const videoGamesStarter = async () => {
   try {
 
     const response = await axios.get(reviewUrl)
-    //console.log(response.data.results)
+    // console.log(response.data.results)
     const data = response.data.results
     
-    for (let i = 0; i < 50; i++)
-    {
-      const score = response.data.results[i].score
-      if (score === 5 ) {
-        renderGames(data[i])
-        //console.log(data[i])
-      }
-        }
+  
+      let sortArray = data.sort((a, b) => b.score - a.score).slice(0, 10)
+     // console.log(sortArray)
+    for (i = 0; i < sortArray.length; i++) {
+      renderGames(sortArray[i], container[i])
+    }
 
   } catch (error) {
     console.log(error)
   }
 }
 
-async function renderGames(results)  {
-  console.log(results.game.api_detail_url)
-  const gameUrl = results.game.api_detail_url
+async function renderGames(game, square) {
+  console.log(square)
+  //console.log(results.game.api_detail_url)
+  const gameUrl = game.game.api_detail_url
   const imageGet = `https://floating-escarpment-65266.herokuapp.com/${gameUrl}${api}`
   let currentGame = {
    
- }
- try {
-  const response = await axios.get(imageGet)
-  currentGame = response.data.results
-   console.log(currentGame)
-   let gameData = `
-  <h1>Title: ${results.game.name}</h1>
-  <img src="${currentGame.image.icon_url}"/>
-  `
-  container.insertAdjacentHTML('beforeend', gameData)
-  return gameData
+  }
+  try {
+    const response = await axios.get(imageGet)
+    currentGame = response.data.results
+    console.log('game', currentGame)
 
+    // nameDiv.innerText = currentGame.name
+    // container.style.backgroundImage = 'url(' + currentGame.image.icon_url + ')'
+    
+    let gameData = `
+  <h1>Title: ${currentGame.name}</h1>
+  <img src="${currentGame.image.icon_url}" class="game-image"/>
+
+  <h3>Genre: ${currentGame.genres[0].name}</h3>
+  `
+    square.insertAdjacentHTML('beforeend', gameData)
+
+  
 
 } catch (error) {
   console.log(error)
 }  
-  }
-
-// generate.addEventListener('click', (e) => {
-//   e.preventDefault()
-//   
-//   videoGamesStarter()
-// })
+ }
+  
+function removeMystery() {
+  for (let i = 0; i < container.length; i++)
+  {
+    holders[i].remove()
+    }
+}
 
 document.getElementById("list-generator").addEventListener('click', (e) => {
- e.preventDefault()
+  e.preventDefault()
+  removeMystery()
   videoGamesStarter()
 })
+
+// const imgClick = renderGames.gameData
+
+// imgClick.addEventListener('click', (e) => {
+//   e.preventDefault()
+
+// })
 
